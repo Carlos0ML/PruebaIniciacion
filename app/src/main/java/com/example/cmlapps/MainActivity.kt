ba664166fc609f468c9911e626585b66e1806d0c
Carlos0ML <92751000+Carlos0ML@users.noreply.github.com>
2021-11-16 17:41:46 +0100

    Inicializando

--- app/src/main/java/com/example/cmlapps/MainActivity.kt
+++ app/src/main/java/com/example/cmlapps/MainActivity.kt
@@ -1,11 +1,43 @@
 package com.example.cmlapps
 
+import android.content.res.Configuration
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.os.PersistableBundle
+import android.util.Log
 
 class MainActivity : AppCompatActivity() {
+
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+            Log.d("Carlos", "Se ha creado una actividad")
     }
+
+    override fun onConfigurationChanged(newConfig: Configuration) {
+        super.onConfigurationChanged(newConfig)
+        Log.d("Carlos","Se ha modificado la configuración")
+    }
+
+    override fun onStart() {
+        super.onStart()
+        Log.d("Carlos","Se ha modificado la configuración")
+    }
+
+    override fun onStop() {
+        super.onStop()
+        Log.d("Carlos","La actividad ha salido del estado visible")
+    }
+
+    override fun onPause() {
+        super.onPause()
+        Log.d("Carlos","La actividad ya no está en primer plano")
+    }
+
+    override fun onDestroy() {
+        super.onDestroy()
+        Log.d("Carlos","La actividad se ha destruido")
+    }
+
 }
